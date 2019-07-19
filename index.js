e6509f579153ec527f3587df852afcc6fdc0f67d
Haixiang6123 <haixiang6123@gmail.com>
2019-07-19 14:00:24 -0700

    Add loading

--- index.js
+++ index.js
@@ -2,23 +2,26 @@
 const axios = require('axios')
 const cheerio = require('cheerio')
 const chalk = require('chalk')
+const ora = require('ora')
 
 const baseURL = 'https://www.abbreviations.com'
 
 // Validate keyword
 if (process.argv.length !== 3) {
-    console.log('Example: wth lol')
+    console.log('Example: wtf-cli lol')
     return
 }
 
 // Get keyword
 const term = process.argv[2]
 console.log(chalk.red('##############################'))
-console.log(chalk.blue('Looking for ' + term + '...'))
+const spinner = ora(chalk.blue('Looking for ' + term + '...')).start();
 
 // Send request
 axios.get(`${baseURL}/${term}`)
     .then(response => {
+        // Stop loading
+        spinner.stop()
         // Get raw html
         const html = response.data
         // Transfer to DOM
