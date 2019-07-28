06ffd92482aa38c397e4dd63913aafbd67046165
Haixiang6123 <haixiang6123@gmail.com>
2019-07-27 21:37:49 -0700

    Add ratings

--- index.js
+++ index.js
@@ -3,8 +3,26 @@ const axios = require('axios')
 const cheerio = require('cheerio')
 const chalk = require('chalk')
 const ora = require('ora')
+const table = require('table').table
 
 const baseURL = 'https://www.abbreviations.com'
+
+const config = {
+    columns: {
+        0: {
+            alignment: 'left',
+            width: 10
+        },
+        1: {
+            alignment: 'center',
+            width: 10
+        },
+        2: {
+            alignment: 'right',
+            width: 10
+        }
+    }
+};
 
 // Validate keyword
 if (process.argv.length !== 3) {
@@ -14,7 +32,6 @@ if (process.argv.length !== 3) {
 
 // Get keyword
 const term = process.argv[2]
-console.log(chalk.red('##############################'))
 const spinner = ora(chalk.blue('Looking for ' + term + '...')).start();
 
 // Send request
@@ -26,16 +43,53 @@ axios.get(`${baseURL}/${term}`)
         const html = response.data
         // Transfer to DOM
         const $ = cheerio.load(html)
+
+        let results = []
         // Get all description on first page
-        $('p.desc').each((index, element) => {
-            // Skip the first one
+        $('tbody p.desc').each((index, element) => {
+            // Skip header
             if (index === 0) {
-              return
+                return
             }
-            console.log(chalk.yellow((index)) + '. ' + $(element).text())
+            // Add description
+            results.push([
+                chalk.yellow(index),
+                $(element).text(),
+                ''
+            ])
         })
-        console.log(chalk.red('##############################'))
+        // Get all ratings
+        $('tbody span.sc').each((index, element) => {
+            // Update ratings
+            results[index][2] = rate(5, cheerio(element).find('span.sf').length)
+        })
+
+        // Print results
+        printResults(results)
     })
     .catch(error => {
         console.log(error)
     })
+
+// Print out formatted results
+function printResults(results) {
+    console.log(chalk.red('##############################'))
+
+    console.log(table(results))
+
+    console.log(chalk.red('##############################'))
+}
+
+// Get ratings styles
+function rate(total, likes) {
+    let ratings = ''
+    for (let i = 0; i < total; i++) {
+        if (i < likes) {
+            ratings += chalk.green('+')
+        }
+        else {
+            ratings += chalk.red('-')
+        }
+    }
+    return ratings
+}
